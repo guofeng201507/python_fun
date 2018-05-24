ec259aa00bbc32d7b220029cefaed9350dbbcd7c
guofeng201507 <guofeng20094@gmail.com>
2018-05-25 00:17:29 +0800

    added chatbot code

--- chatbot/fengbot.py
+++ chatbot/fengbot.py
@@ -1,14 +1,24 @@
 # fengbot.py
 from nltk.chat.util import Chat, reflections
+
 pairs = [
-    [
+    (
         r'hi',
-        ['hello', 'kamusta', 'mabuhay',]
-    ],
+        ['hello', 'kamusta', 'mabuhay', ]
+    ),
+
+    (
+        r'Which stock',
+        ['tencent', 'Alibaba']
+    )
 ]
+
+
 def hugot_bot():
     print("Hi how can I help you today?")
     chat = Chat(pairs, reflections)
     chat.converse()
+
+
 if __name__ == "__main__":
     hugot_bot()
